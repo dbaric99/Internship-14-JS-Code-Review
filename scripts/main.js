4d9fc54dc50afa18c6d26383f3f717115e3c3763
dbaric99 <damjana.baric1999@gmail.com>
2023-03-02 22:31:41 +0100

    Displayed code to the screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
-const baseUrl = "https://homework-server1.onrender.com";
-const key = "dbaric99";
+const baseUrl = 'https://homework-server1.onrender.com';
+const key = 'dbaric99';
+const codeContainer = document.querySelector('.code-container');
 
 console.log("something is happening");
 
@@ -14,5 +15,18 @@ async function fetchCodeBlocks() {
 }
 
 fetchCodeBlocks().then(codeBlock => {
-    console.log(codeBlock)
+    var codeLines = codeBlock.code.split('\n');
+    codeLines.forEach((line, index) => {
+        const lineIndex = document.createElement('span');
+        lineIndex.classList.add = 'code-line__index';
+        lineIndex.innerHTML = index;
+
+        const element = document.createElement('div');
+        element.classList.add = 'code-line';
+        element.appendChild(lineIndex);
+        element.innerHTML = line.replace(/ /g, '&nbsp;');
+
+        codeContainer.appendChild(element);
+    })
+    console.log(codeLines);
 });
