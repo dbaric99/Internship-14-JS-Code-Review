037f951583edcd2f0dbafeab2d91025fed859fd8
dbaric99 <damjana.baric1999@gmail.com>
2023-03-02 21:17:50 +0100

    Fetched code block from server

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,18 @@
-const baseUrl = "https://homework-server1.onrender.com/";
+const baseUrl = "https://homework-server1.onrender.com";
 const key = "dbaric99";
+
+console.log("something is happening");
+
+async function fetchCodeBlocks() {
+    const method = 'GET';
+    const headers = { key };
+    const options = {method, headers};
+
+    const response = await fetch(`${baseUrl}/code`, options);
+    const codeBlock = await response.json();
+    return codeBlock;
+}
+
+fetchCodeBlocks().then(codeBlock => {
+    console.log(codeBlock)
+});
